29420858e2244b2ee280275e37b0bb4753e4870d
Hung Nguyen <hungnt89@gmail.com>
2020-07-14 17:29:07 +0700

    AnswerList, margin, highlight, shuffle, lodash

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <template>
   <div id="app">
     <Header />
-    <QuestionBox />
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col sm="6" offset="3">
+          <QuestionBox v-if="questions.length" :currentQuestion="questions[index]" :next="next" />
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
@@ -14,6 +20,24 @@ export default {
   components: {
     Header,
     QuestionBox
+  },
+  data() {
+    return {
+      questions: [],
+      index: 0
+    };
+  },
+  methods: {
+    next() {
+      this.index++;
+    }
+  },
+  mounted: function() {
+    fetch("https://opentdb.com/api.php?amount=10&category=11&type=multiple", {
+      method: "get"
+    })
+      .then(res => res.json())
+      .then(qData => (this.questions = qData.results));
   }
 };
 </script>
--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -1,11 +1,86 @@
 <template>
   <div class="question-box-container">
-    <b-jumbotron
-      header="BootstrapVue"
-      lead="Bootstrap v4 Components for Vue.js 2"
-    >
-      <p>For more information visit website</p>
-      <b-button variant="primary" href="#">More Info</b-button>
+    <b-jumbotron>
+      <template v-slot:lead>{{ currentQuestion.question }}</template>
+
+      <hr class="my-4" />
+
+      <b-list-group>
+        <b-list-group-item
+          v-for="(answer, index) in answers"
+          :key="index"
+          @click="selectAnswer(index)"
+          :class="[selectedIndex === index ? 'selected' : '']"
+          >{{ answer }}</b-list-group-item
+        >
+      </b-list-group>
+
+      <b-button variant="primary" href="#">Submit</b-button>
+      <b-button @click="next" variant="success" href="#">Next</b-button>
     </b-jumbotron>
   </div>
 </template>
+
+<script>
+import _ from "lodash";
+export default {
+  props: {
+    currentQuestion: Object,
+    next: Function,
+  },
+  data() {
+    return {
+      selectedIndex: null,
+      shuffledAnswers: [],
+    };
+  },
+  methods: {
+    selectAnswer(index) {
+      this.selectedIndex = index;
+      console.log(index);
+    },
+    shuffleAnswers() {
+      let answers = [
+        ...this.currentQuestion.incorrect_answers,
+        this.currentQuestion.correct_answer,
+      ];
+      this.shuffledAnswers = _.shuffle(answers);
+    },
+  },
+  watch: {
+    currentQuestion() {
+      this.selectedIndex = null;
+      this.shuffleAnswers();
+    },
+  },
+  computed: {
+    answers() {
+      let answers = [...this.currentQuestion.incorrect_answers];
+      answers.push(this.currentQuestion.correct_answer);
+      return answers;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.list-group {
+  margin-bottom: 1rem;
+}
+.btn {
+  margin: 0 6px;
+}
+.list-group-item:hover {
+  background-color: #eee;
+  cursor: pointer;
+}
+.selected {
+  background-color: lightblue;
+}
+.correct {
+  background-color: lightgreen;
+}
+.incorrect {
+  background-color: lightcoral;
+}
+</style>
